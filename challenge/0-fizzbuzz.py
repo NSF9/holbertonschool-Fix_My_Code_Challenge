8e364b456ba92c4b972d2acce92ebcdec6332bcf
NSF9 <alzhraniii999@gmail.com>
2026-05-02 00:49:43 +0300

    Refactor FizzBuzz to Print Square with Character #

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -7,7 +7,6 @@ import sys
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
-
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
@@ -18,8 +17,7 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-            continue
-        if (i % 3) == 0:
+        elif (i % 3) == 0:
             tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
@@ -34,6 +32,5 @@ if __name__ == '__main__':
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
-
     number = int(sys.argv[1])
     fizzbuzz(number)
